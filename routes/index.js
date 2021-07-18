0820e649ee56b84dc89c8839446198c2be640303
Nayiem <info@hodlercompany.com>
2021-07-18 22:52:56 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,38 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "Voyager Park",
+    title: "Home - Voyager Park",
     routename: "home",
     csrfToken: req.csrfToken()
   });
 });
 
-
-/* GET support pages. 
-router.get("/support", function (req, res, next) {
-  res.render("support", {
-    title: "Qredit - Support",
-    routename: "support",
+router.get("/accommodation", function (req, res, next) {
+  res.render("accommodation", {
+    title: "Accommodation - Voyager Park",
+    routename: "accommodation",
     csrfToken: req.csrfToken()
   });
 });
-*/
+
+router.get("/rentals-and-activities", function (req, res, next) {
+  res.render("rentals-and-activities", {
+    title: "Rentals and Activities - Voyager Park",
+    routename: "rentals-and-activities",
+    csrfToken: req.csrfToken()
+  });
+});
+
+router.get("/deluxeigloo", function (req, res, next) {
+  res.render("deluxeigloo", {
+    title: "Deluxe Glass - Voyager Park",
+    routename: "deluxeigloo",
+    csrfToken: req.csrfToken()
+  });
+});
+
+
 
 module.exports = router;
